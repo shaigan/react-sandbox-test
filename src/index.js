e5260ca75a332d72b41c9a9966b450ef01a48df3
shaigan <shaigan@users.noreply.github.com>
2019-07-16 12:40:36 +0200

    test
    test

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+function Test() {
+  return (
+    <div>
+      <h3>test</h3>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Test />
     </div>
   );
 }
